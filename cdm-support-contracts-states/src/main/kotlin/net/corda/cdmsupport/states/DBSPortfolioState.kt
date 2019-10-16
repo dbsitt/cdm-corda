a8f7cb44f5ef6a27c5a34afa31fa0b85afa3e9b9
Cuong <cuongnc01655@gmail.com>
2019-10-16 23:23:22 +0800

    add DBSportfoliostate

---
+++ cdm-support-contracts-states/src/main/kotlin/net/corda/cdmsupport/states/DBSPortfolioState.kt
@@ -0,0 +1,23 @@
+package net.corda.cdmsupport.states
+
+import com.regnosys.rosetta.common.serialisation.RosettaObjectMapper
+import net.corda.cdmsupport.CDMEvent
+import net.corda.core.contracts.BelongsToContract
+import net.corda.core.contracts.LinearState
+import net.corda.core.contracts.UniqueIdentifier
+import net.corda.core.identity.Party
+import org.isda.cdm.Portfolio
+
+@BelongsToContract(CDMEvent::class)
+data class DBSPortfolioState(
+        val portfolioJson: String,
+        val executionRef: String,
+        val workflowStatus : String,
+        override val participants: List<Party>,
+        override val linearId: UniqueIdentifier = UniqueIdentifier()) : LinearState {
+
+    fun portfolio(): Portfolio {
+        val rosettaObjectMapper = RosettaObjectMapper.getDefaultRosettaObjectMapper()
+        return rosettaObjectMapper.readValue<Portfolio>(portfolioJson, Portfolio::class.java)
+    }
+}
